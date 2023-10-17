e00f665b447af889da64ab1bf7026bb1685c7018
12ball <pub@janie.page>
2023-10-16 21:10:26 -0500

    Begin

--- install.sh
+++
@@ -1,7 +0,0 @@
-set -e
-printf 'source "https://rubygems.org/"\n\ngroup :development do\n  gem "peppermint", git: "https://github.com/janie314/peppermint"\nend\n' >Gemfile
-printf '# delete one of these, then save and quit...\nrequire "peppermint/rake/ruby"\nrequire "mutiny-devel/rake/rust"\n' >Rakefile
-vi Rakefile
-bundle
-bundle exec rake update_devel_config
-echo 'All done!'
