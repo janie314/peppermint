661b57f3778cd267518ec2543694d8a327e1b2aa
12ball <pub@janie.page>
2023-12-30 12:37:22 -0600

    1703961442

---
+++ lib/peppermint/langEnv/deno.rb
@@ -0,0 +1,24 @@
+module Peppermint
+  class Deno < LangEnv
+    def install_lang
+      system "command -v deno || curl -fsSL https://deno.land/x/install/install.sh | sh"
+    end
+
+    def cp_files
+      # TODO copy to gitroot, not pwd
+    end
+
+    def fmt
+      system <<~SHELL
+        find . \\( -not -path '*target*' \\) \
+          -and \\( -not -path '*node_modules*' \\) \
+          -and \\( -not -path '*/doc' \\) \
+          -and \\( -name '*.json' -o -name '*.md' -o -name '*.js' \
+            -o -name '*.ts' -o -name '*.jsx' -o -name '*.tsx' \\) \
+          -and \\( -not -name 'project.json' \\) \
+          -and \\( -not -name 'personal.json' \\) \
+          -and -type f | xargs -r deno fmt
+      SHELL
+    end
+  end
+end
--- lib/peppermint/langEnv/ruby.rb
+++ lib/peppermint/langEnv/ruby.rb
@@ -1,4 +1,47 @@
+require "rspec/core/rake_task"
+require "yard"
+require_relative "gwenGPT"
 
 module Peppermint
+  class Ruby < LangEnv
+    def initialize
+      RSpec::Core::RakeTask.new(:spec)
 
+      RuboCop::RakeTask.new do |t|
+        t.requires << "rubocop-rake"
+      end
+
+      YARD::Rake::YardocTask.new do |t|
+        t.files = ["lib/**/*.rb"]
+        t.options = ["--any", "--extra", "--opts"]
+        t.stats_options = ["--list-undoc"]
+      end
+    end
+
+    def install_lang
+      res = system("git", "clone", "https://github.com/rbenv/rbenv.git",
+        File.join(ENV["HOME"], ".rbenv"))
+      if !res
+        puts "problem..."
+        return
+      end
+      res = system("git", "clone", "https://github.com/rbenv/ruby-build.git",
+        File.join(ENV["HOME"], ".rbenv", "plugins", "ruby-build"))
+      if !res
+        puts "problem..."
+        return
+      end
+      puts "Now put this in your bashrc:\n", <<~BLOCK
+        eval "$(~/.rbenv/bin/rbenv init - bash)"
+      BLOCK
+    end
+
+    # TODO copy to gitroot, not pwd
+    def cp_files
+      [".solargraph.yml", ".rubocop.yml", ".standard.yml", ".rspec",
+        ".ruby-version"].each do |filename|
+        FileUtils.cp (File.expand_path File.join __dir__, "..", "..", "..", filename), Dir.pwd
+      end
+    end
+  end
 end
