0df4a6d465152b3c0077d270981ce021a9c0c889
12ball <pub@janie.page>
2023-12-31 21:01:11 -0600

    1704078071

--- lib/peppermint/git.rb
+++ lib/peppermint/git.rb
@@ -9,5 +9,10 @@ module Peppermint
     def self.clone_repo(url, target_dir)
       system("git", "clone", url, target_dir)
     end
+
+    # is the target directory a git repo?
+    def self.is_git_repo(target_dir)
+      File.join(target_dir, ".git").dir?
+    end
   end
 end
